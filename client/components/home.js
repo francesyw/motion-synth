ec89f678d5de42121f8af9641cf62b2ffe91a5bd
Frances Wang <francesyuanwang@gmail.com>
2018-05-15 10:52:03 -0400

    clean up

--- client/components/home.js
+++ client/components/home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
 import Content from './content';
-// import { connect } from 'react-redux';
 
 const socket = io(window.location.origin);
 
